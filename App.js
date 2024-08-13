7bc6b7b718b826b23e19a9d07cf5716feed586cd
haroonkhan29 <haroon77.afridi@gmail.com>
2024-08-13 18:56:16 +0500

    update

--- App.js
+++ App.js
@@ -20,8 +20,7 @@ const pettyCashRoutes = require('./routes/pettyCashRoutes');
 const inventoryRoutes = require('./routes/inventoryRoutes'); 
 const app = express();
 
-
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
